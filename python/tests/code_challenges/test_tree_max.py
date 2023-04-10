4b1b1331c96722b4c242f2717a082d11f3139984
Ethan Albers <ekalbers22@gmail.com>
2023-04-10 18:54:20 -0400

    challenge 16

--- python/tests/code_challenges/test_tree_max.py
+++ python/tests/code_challenges/test_tree_max.py
@@ -2,7 +2,7 @@ import pytest
 from code_challenges.trees.binary_tree import BinaryTree, Node
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_max_val():
     tree = BinaryTree()
     tree.root = Node(10)
@@ -12,4 +12,24 @@ def test_max_val():
     actual = tree.find_maximum_value()
     expected = 30
 
+    assert actual == expected
+
+@pytest.mark.skip('TODO')
+def test_max_happy():
+    tree = BinaryTree()
+    tree.root = Node()
+
+
+def test_max_break():
+    tree = BinaryTree()
+    actual = tree.find_maximum_value()
+    expected = None
+    assert actual == expected
+
+
+def test_max_edge():
+    tree = BinaryTree()
+    tree.root = Node(10)
+    actual = tree.find_maximum_value()
+    expected = 10
     assert actual == expected
